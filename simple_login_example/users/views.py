fec77342d58ae6900708369ffacda23f010ffcb1
al1kite <widekdus62@gmail.com>
2022-11-25 16:35:51 +0900

    7주차 실습: render

--- simple_login_example/users/views.py
+++ simple_login_example/users/views.py
@@ -15,24 +15,30 @@ def login(request):
                 'username': 'python',
                 'password': 'django'
         }
-
+        # if(request.method == 'GET'):
+                 # username = request.GET['username']
+                # password = request.GET['password']
         if(request.method == 'GET'):
-                username, password = None, None
-                try:
-                        username = request.GET['username']
-                        password = request.GET['password']
-                except MultiValueDictKeyError:
-                        if username is None:
+                username = request.GET.get('username')
+                password = request.GET.get('password')
+                # # try:
+                #         username = request.GET['username']
+                #         password = request.GET['password']
+                # except MultiValueDictKeyError:
+                #         if username is None:
+                #                 return HttpResponse('유저 아이디를 입력해주세요')
+                #         if password is None:
+                #             return HttpResponse('유저 비밀번호를 입력해주세요')        
+                if username is None:
                                 return HttpResponse('유저 아이디를 입력해주세요')
-                        if password is None:
-                                return HttpResponse('유저 비밀번호를 입력해주세요')        
-
+                if password is None:
+                                return HttpResponse('유저 비밀번호를 입력해주세요')
                 if(username != user_data['username']):
                         return HttpResponse('유저 아이디가 올바르지 않습니다.')
 
                 if(password != user_data['password']):
                         return HttpResponse('유저 비밀번호가 올바르지 않습니다.')
                 
-                return HttpResponse('로그인 성공!!')
+                return render(request, 'users/login.html')
 
         return HttpResponse()
